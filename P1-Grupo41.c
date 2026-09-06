cd1a14a5c6c7f73d206f1d82df150de4f3dabb7d
AltsGamer530 <gamervalentin530@gmail.com>
2026-09-06 17:46:20 -0300

    Update P1-Grupo41.c

--- P1-Grupo41.c
+++ P1-Grupo41.c
@@ -1,8 +1,26 @@
-#include 
+/*Consideraciones a tener en cuenta:
+El número estimado de electores en el archivo es de 2000.
+Las listas deber´an estar ordenadas de menor a mayor respecto del D.N.I..
+Para la lista secuencial, en la b´usqueda binaria la consigna a utilizar ser´a bisecci´on, l´ımite inferior inclusivo, l´ımi-
+te superior inclusivo, testigo a izquierda y segmento mas grande a la izquierda. Para ella no se utilizar´a ning´un
+elemento ficticio para indicar inicio o fin de lista.
+La confirmaci´on de la baja en la rutina de baja debe realizarse por c´odigo, es decir comparando toda la nupla (en
+todas las estructuras) .
+La politica de reemplazo en la baja de los ´Arboles cuando el nodo tiene dos hijos es el menor de los mayores y el
+reemplazo deber´a realizarse con copia de datos.
+El valor utilizado como +infinito en LVO es el n´umero 999.999.999.
+El D.N.I. es un entero.
+El campo Nombre y apellido es una secuencia de hasta 50 caracteres.
+El campo domicilio es una secuencia de hasta 80 caracteres.
+El C´odigo Postal es un entero.
+El N´umero de mesa es un entero.
+El Circuito es un entero.
+El ingreso de datos no debe ser sensible a may ´usculas y min ´usculas, esto significa por ejemplo que SOSA = SoSa
+= sosa.*/
 
 
-
-
+#include <stdio.h>
+#define INFINITO_LVO 999999999
 typedef struct {
     long dni;
     char nombreApellido[51];
@@ -11,3 +29,18 @@ typedef struct {
     int mesa;
     int circuito;
 } Elector;
+
+typedef struct {
+    float costoIngreso;
+    float costoBaja;
+    float costoConsulta;
+} Costos;
+
+/*Lista Vinculada Ordenada con terminaci´on dada por contenido (+ infinito) (LVO)*/
+
+typedef struct NodoLVO{
+    Elector dato;
+    struct NodoLVO *siguien;
+} NodoLVO;
+
+/*Lista Secuencial Ordenada con b´usqueda binaria (LSOBB)*/
