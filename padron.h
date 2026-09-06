b56471886747278ca2455705e592b1fec421fdf7
ZTLin <linzhontinfranco@outlook.com>
2026-09-06 19:19:04 -0300

    agregar comparacion de padrones en padron.h

--- padron.h
+++ padron.h
@@ -46,29 +46,39 @@ char* getDomicilio( const padron *P) {
 }
 
 // ============ SETTERS ============
-void setDNI(padron *P, int dni) {
+void setDNI(padron *P, const int dni) {
     P->dni = dni;
 }
 
-void setCPostal(padron *P, int cPostal) {
+void setCPostal(padron *P, const int cPostal) {
     P->cPostal = cPostal;
 }
 
-void setMesa(padron *P, int mesa) {
+void setMesa(padron *P, const int mesa) {
     P->mesa = mesa;
 }
 
-void setCircuito(padron *P, int circuito) {
+void setCircuito(padron *P, const int circuito) {
     P->circuito = circuito;
 }
 
-void setNombreApellido(padron *P,  char *nombre) {
+void setNombreApellido(padron *P, const char *nombre) {
     strcpy(P->nombreApellido, nombre);
 }
 
-void setDomicilio(padron *P,  char *domicilio) {
+void setDomicilio(padron *P, const char *domicilio) {
     strcpy(P->domicilio, domicilio);
 }
 
+// ============== PMI ==============
+bool padron_sonIguales(const padron *a, const padron *b) {
+    if (a->dni != b->dni) return false;
+    if (a->cPostal != b->cPostal) return false;
+    if (a->mesa != b->mesa) return false;
+    if (a->circuito != b->circuito) return false;
+    if (stricmp(a->nombreApellido, b->nombreApellido) != 0) return false;
+    if (stricmp(a->domicilio, b->domicilio) != 0) return false;
+    return true;
+}
 
 #endif // PADRON_H_INCLUDED
