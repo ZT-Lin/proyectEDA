913d65f4c97d140a34ab9d3207d3bb706537fef4
ZTLin <linzhontinfranco@outlook.com>
2026-09-17 12:37:12 -0300

    LSO parcialmente terminado

--- LSOBB.h
+++ LSOBB.h
@@ -7,43 +7,76 @@
 
 typedef struct{
     padron datos[LSOMAX];
-    int cantidad;
+    int cantidad = 0;
 }LSOBB;
 
 void LSOBB_inicializar(LSOBB *lista) {
     lista->cantidad = 0;
 }
 
+int vaciaLSO(LSOBB l){
+    return (l.cantidad == 0);
+}
+
+int llenaLSO(LSOBB l){
+    return (l.cantidad > LSOMAX);
+}
+
+int localizarLSO(LSOBB l, const padron p){
+    int li = 0; //inclusivo
+    int ls = l.cantidad; //inclusivo
+    int medio = 0;
+
+    while(li<ls){
+            medio = (li+ls)/2;
+            if( getDNI(l.datos[medio]) < getDNI(p) ){
+                li = medio+1;
+            }else{
+                ls = medio-1;
+            }
+    }
+
+    if ( getDNI(l.datos[medio]) == getDNI(p) ){
+        return medio;
+    }else{
+        return -1; // no encontrado
+    }
+}
 
 int altaLSO(LSOBB *l, const padron elector){
-    if (l->cantidad >= LSOMAX) {
-        return -1;  // error: lista llena
+    if ( llenaLSO(*l) ) { return -1;} // lista llena
+
+    int posicion = localizarLSO(*l,elector);
+    if (posicion == -1){ return -1;} //no encontrado
+
+    //dar alta en posicion x
+
+    int fin = l->cantidad;
+    int costo = 0;
+    while (posicion < fin){
+        l->datos[fin+1] = l->datos[fin];
+        costo+1;
     }
-    int li = 0; //inclusivo
-    int ls = l->cantidad; //inclusivo
-    int medio = 0;
-    int celdas_visitadas = 0;
-
-    while( li<ls ){
-        medio = floor( (li + ls)/2 ); //testigo a izquierda
-
-        //aca visita una celda
-        if( getDNI(l->datos[medio]) <  getDNI(elector) ){
-            li = medio+1;
-        }else{
-            ls = medio;
-        }
-        celdas_visitadas++;
-    }
-
-    /*
-    cuando sale del while: tiene una lista de tamanio 1
-    con lista.contenido.dni >= elector.dni
-    hay que insertar por en celda izquierda
-    */
-
-
-
+    l->datos[posicion] = elector;
+    return costo;
 }
+
+int bajaLSO(LSOBB *l, const padron elector){
+    if ( llenaLSO(*l) ) { return -1;} // lista llena
+
+    int posicion = localizarLSO(*l,elector);
+    if (posicion == -1){ return -1;} //no encontrado
+
+    //dar alta en posicion x
+
+    int fin = l->cantidad;
+    int costo = 0;
+    while (posicion <= fin){
+        l->datos[fin+1] = l->datos[fin];
+        costo+1;
+    }
+    return costo;
+}
+
 
 #endif // LSO_H_INCLUDED
