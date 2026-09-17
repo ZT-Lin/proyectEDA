d4058d27c482c1e0d919eb1d10132b89e9f11b7d
ZTLin <linzhontinfranco@outlook.com>
2026-09-17 13:28:25 -0300

    modificacion no importante

---
+++ leearch.h
@@ -0,0 +1,8 @@
+#ifndef LEEARCH_H_INCLUDED
+#define LEEARCH_H_INCLUDED
+
+FILE *f = fopen("Operaciones_Padron.txt", "r");
+
+
+
+#endif // LEEARCH_H_INCLUDED
--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int cargar_struct(LSOBB lso, ){
+    int ibuffer=0;
+    FILE *operaciones = fopen( "Operaciones_Padron.txt", "r");
+
+    if (fp==NULL){
+            return -1;
+    }
+
+    while( !feof(operaciones) ){
+
+            fscanf("Operaciones_Padron.txt", "%d",ibuffer );
+
+    }
+}
 
 int main(){
-    printf("Hello world!\n");
-    return 0;
+
+
 }
