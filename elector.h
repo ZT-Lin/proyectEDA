4ff8cbc6dcebccf48e82bf0cd2fbc05196f8045b
ZTLin <linzhontinfranco@outlook.com>
2026-09-18 17:09:51 -0300

    corregir nombre: padron -> elector
    fase 1 terminado

---
+++ elector.h
@@ -0,0 +1,85 @@
+#ifndef ELECTOR_H_INCLUDED
+#define ELECTOR_H_INCLUDED
+
+#include <stdio.h>
+#include <string.h>
+
+
+typedef struct{
+    int dni, cPostal, mesa, circuito;
+    char nombreApellido[51];
+    char domicilio[81];
+}elector;
+
+void inicializarElector (padron *Elector){
+    Elector->dni = 0;
+    strcpy(Elector->nombreApellido, "indefinido");
+    strcpy(Elector->domicilio, "indefinido");
+    Elector->cPostal = 0;
+    Elector->mesa = 0;
+    Elector->circuito = 0;
+}
+
+// ============ GETTERS ============
+int getDNI( const elector Elector) {
+    return P.dni;
+}
+
+int getCPostal( const elector Elector) {
+    return P.cPostal;
+}
+
+int getMesa( const elector Elector) {
+    return P.mesa;
+}
+
+int getCircuito( const elector Elector) {
+    return P.circuito;
+}
+
+char* getNombreApellido( const elector Elector) {
+    return P.nombreApellido;
+}
+
+char* getDomicilio( const elector Elector) {
+    return P.domicilio;
+}
+
+// ============ SETTERS ============
+void setDNI(elector *Elector, const int dni) {
+    Elector->dni = dni;
+}
+
+void setCPostal(elector *Elector, const int cPostal) {
+    Elector->cPostal = cPostal;
+}
+
+void setMesa(elector *Elector, const int mesa) {
+    Elector->mesa = mesa;
+}
+
+void setCircuito(elector *Elector, const int circuito) {
+    Elector->circuito = circuito;
+}
+
+void setNombreApellido(elector *Elector, const char *nombre) {
+    strcpy(Elector->nombreApellido, nombre);
+}
+
+void setDomicilio(elector *Elector, const char *domicilio) {
+    strcpy(Elector->domicilio, domicilio);
+}
+
+// ============== PMI ==============
+bool elector_sonIguales(const elector *a, const elector *b) {
+    if (a->dni != b->dni) return false;
+    if (a->cPostal != b->cPostal) return false;
+    if (a->mesa != b->mesa) return false;
+    if (a->circuito != b->circuito) return false;
+    if (stricmp(a->nombreApellido, b->nombreApellido) != 0) return false;
+    if (stricmp(a->domicilio, b->domicilio) != 0) return false;
+    return true;
+}
+
+
+#endif // ELECTOR_H_INCLUDED
