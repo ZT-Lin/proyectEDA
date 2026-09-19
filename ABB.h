f404527480af67b94624abb0e10565adc19a4524
ZTLin <linzhontinfranco@outlook.com>
2026-09-19 00:08:15 -0300

    ABB: float altaABB(ABB *abb, const elector Elector)

--- ABB.h
+++ ABB.h
@@ -48,21 +48,58 @@ NodoABB* localizarABB(const ABB *abb, const elector Elector, int *costo) {
     }
 
     int dni = Elector.dni;
-    NodoABB *actual = abb->raiz;
+    NodoABB *padre = abb->raiz;
+    NodoABB *hijo = abb->raiz;
 
-    while (actual != NULL) {
+    while (hijo != NULL) {
         (*costo)++;
 
-        if (dni == actual->Elector.dni) {
-            return actual; // v == x? -> encontrado
-        } else if (dni < actual->Elector.dni) {
-            actual = actual->izq; // v < x? -> - A1 menor
+        if (dni == hijo->Elector.dni) {
+            return hijo; // v == x? -> encontrado
+        }
+
+        padre = hijo;
+        if (dni < hijo->Elector.dni) {
+            hijo = hijo->izq; // v < x? -> - A1 menor
         } else {
-            actual = actual->der; // v > x? -> A2 - mayor
+            hijo = hijo->der; // v > x? -> A2 - mayor
         }
     }
 
-    return NULL;
+    return padre;
 }
 
+float altaABB(ABB *abb, const elector Elector){
+    float costo = 0.0;
+    NodoABB *nuevo = crearNodoABB(Elector);
+    if (nuevo == NULL) {return -1.0;} //espacio insuficiente
+
+     // arbol vacio
+    if (abb->raiz == NULL) {
+        abb->raiz = nuevo;
+        costo += 0.5;
+        return costo;
+    }
+
+    // si el elector que me devuelve tiene mismo dni
+    int costoBusqueda = 0;
+    NodoABB *interno = localizarABB(abb, Elector, &costoBusqueda);
+    if( interno->Elector.dni == nuevo->Elector.dni ) {
+            free(nuevo);
+            return -2.0;
+    }
+
+    // no es mismo dni + arbol no vacio => interno = posicion a insertar segun dni
+    if (Elector.dni < interno->Elector.dni) {
+        interno->izq = nuevo;
+    } else {
+        interno->der = nuevo;
+    }
+
+    costo += 0.5;
+    return costo;
+}
+
+float bajaABB(ABB *abb, const elector Elector){}
+float evocarABB(ABB *abb, const elector Elector){}
 #endif // ABB_H_INCLUDED
