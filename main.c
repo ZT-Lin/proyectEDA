54f96aa70bb950f63589442c877b0e8bc33ca5a7
ZTLin <linzhontinfranco@outlook.com>
2026-09-21 13:53:32 -0300

    detalles fixeados

--- main.c
+++ main.c
@@ -1,3 +1,37 @@
+/*
+Funcion de costo:
+    alta y baja:
+        LSOBB: cantidad de corrimiento de la celda, costo = 1.
+        ABB y LVO: modificacion de punteros, costo = 0.5
+
+        La politica de reemplazo en la baja de los Árboles:
+            cuando el nodo tiene dos hijos es el menor de los mayores y el
+            reemplazo deberá realizarse con copia de datos. se deberá sumar
+            un costo más (1).
+
+     evocar:
+        todos: cantidad total de celdas consultadas, un punto (1) por cada celda.
+
+====================ANALISIS====================
+ALTA:
+    LSOBB: una vez encontrado la posicion a insertar segun funcion LOCALIZAR,
+        se realiza n veces el corrimiento de atras hacia adelante hasta el posicion
+        que corresponde con FOR(iteracion). Luego, copia los datos.
+        => pertenece a O(n)
+
+    ABB:
+        una vez encontrado el nodo segun LOCALIZAR,
+
+BAJA
+
+
+EVOCAR
+
+==============================================
+*/
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -84,7 +118,7 @@ int main(){
     initLSOBB(&lista_secuencial_ordenada);
 
     lista_lvo = inicializar_lvo();
-    
+
     ABB arbol_binario_busqueda;
     initABB(&arbol_binario_busqueda);
 
@@ -202,6 +236,7 @@ int main(){
                 break;
             } // mostrar estructura ABB
             case '4': {
+                system("cls");
                 promedio(&cAlta_lsobb_ex);   promedio(&cAlta_lsobb_fr);
                 promedio(&cBaja_lsobb_ex);   promedio(&cBaja_lsobb_fr);
                 promedio(&cEvocar_lsobb_ex); promedio(&cEvocar_lsobb_fr);
@@ -433,6 +468,7 @@ void mostrarABB(NodoABB *nodo, int *mostrados, int total, int *pagina){
     if (nodo == NULL) return;
 
     // nodo actual
+    // valor
     printf("------------------------------------------------------------\n");
     printf("DNI:\t\t%d\n", nodo->root.dni);
     printf("Nombre:\t\t%s\n", nodo->root.nombreApellido);
@@ -466,8 +502,8 @@ void mostrarABB(NodoABB *nodo, int *mostrados, int total, int *pagina){
         system("cls");
     }
 
-    // preorden: izquierdo, luego derecho
+    // preorden: valor(mostrado anteriormente) -> izquierdo -> derecho
     // avanzar
     mostrarABB(nodo->izq, mostrados, total, pagina);
     mostrarABB(nodo->der, mostrados, total, pagina);
-}   
+}
